7a30a3ab24b0c53be1556c4abdfc3a4c04f3074f
jonathastavares <jonathastavares@id.uff.br>
2021-06-14 14:11:50 -0300

    Change model table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_09_151935) do
+ActiveRecord::Schema.define(version: 2021_06_14_160829) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2021_06_09_151935) do
     t.datetime "event_date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
