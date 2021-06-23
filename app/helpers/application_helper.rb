fb19ed629937f25c9bb282dda718c4b3bdba0735
jonathastavares <jonathastavares@id.uff.br>
2021-06-23 16:25:43 -0300

    Fixed associations bugs, started design a simple layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,26 @@ module ApplicationHelper
           # rubocop:enable Metrics/LineLength
         end
     end
+
+    def show_create_event_button
+      return unless current_user
+      (link_to 'Create new event', new_event_path, class: 'nav-link text-info').to_s.html_safe
+    end
+
+    def show_user_name(current_user)
+      if signed_in?
+        "<li class='nav-item'>
+          <div class='nav-link text-white'>
+            Logged in as [ <strong  >#{link_to current_user.username, profile_path(current_user.id)} </strong>]
+          </div>
+        </li>
+        <li class='nav-item'>
+           #{link_to 'logout', destroy_user_session_path, class: 'nav-link text-white', method: :delete}
+        </li>".html_safe
+      else
+        "<li class='nav-item'>
+          #{link_to 'Log in / Sign up', new_user_registration_path, class: 'nav-link text-white'}
+        </li>".html_safe
+      end
+    end
 end
